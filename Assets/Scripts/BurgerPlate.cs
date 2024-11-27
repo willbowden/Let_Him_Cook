4023e612cb22816afd688236451e99761637c96c
willbowden <willbowden03@yahoo.co.uk>
2024-11-27 11:59:21 +0800

    Push to fix checkPlate. Burger Assembly in progress

--- Assets/Scripts/BurgerPlate.cs
+++ Assets/Scripts/BurgerPlate.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
@@ -6,33 +9,112 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class BurgerPlate : MonoBehaviour
 {
   [SerializeField]
-  private Transform nextAttachPoint;
-  private List<GameObject> contents = new();
-  private XRSocketInteractor socket;
+  // private Transform nextAttachPoint;
+  private Stack<GameObject> contents = new();
+  [SerializeField]
+  private GameObject socketPrefab;
+  [SerializeField]
+  private GameObject burgerStack;
+
+  private GameObject bottomSocket;
 
   void Start()
   {
-    if (nextAttachPoint == null)
-    {
-      Debug.LogError("BurgerPlate does not have the required intial attach point defined. Disabling this plate.");
-      this.enabled = false;
-    }
+    // if (nextAttachPoint == null)
+    // {
+    //   Debug.LogError("BurgerPlate does not have the required intial attach point defined. Disabling this plate.");
+    //   this.enabled = false;
+    // }
 
-    socket = GetComponent<XRSocketInteractor>();
-    socket.selectEntered.AddListener(IngredientAdded);
-    socket.selectExited.AddListener(IngredientRemoved);
-    socket.attachTransform = nextAttachPoint;
+    bottomSocket = transform.Find("StackSocket").gameObject;
+
+    var socketInteractor = bottomSocket.GetComponent<XRSocketInteractor>();
+    socketInteractor.selectEntered.AddListener(IngredientAdded);
+    socketInteractor.selectExited.AddListener(IngredientRemoved);
+    // socketInteractor.attachTransform = nextAttachPoint;
+
   }
 
   private void IngredientAdded(SelectEnterEventArgs args)
   {
-    GameObject obj = args.interactableObject.transform.gameObject;
-    
+    GameObject ingredient = args.interactableObject.transform.gameObject;
+    ingredient.transform.SetParent(burgerStack.transform);
+
+    Vector3 offset = new(0, ingredient.GetComponent<MeshRenderer>().bounds.size.y, 0);
+
+    GameObject newSocket = Instantiate(socketPrefab, ingredient.transform);
+    newSocket.transform.position += offset;
+    newSocket.name = "StackSocket";
+
+    var newSocketInteractor = newSocket.GetComponent<XRSocketInteractor>();
+    newSocketInteractor.attachTransform = newSocket.transform;
+    newSocketInteractor.selectEntered.AddListener(IngredientAdded);
+    newSocketInteractor.selectExited.AddListener(IngredientRemoved);
+
+    print(string.Format("Added new socket. Selecting: {0}, Hovering: {1}", newSocketInteractor.allowSelect, newSocketInteractor.allowHover));
+
+    if (contents.Count > 0)
+    {
+      GameObject previousIngredient = contents.Peek();
+      GameObject socket = previousIngredient.transform.Find("StackSocket").gameObject;
+     
+      EnableSocket(socket);
+    }
+    else
+    {
+      DisableSocket(bottomSocket);
+    }
+
+    contents.Push(ingredient);
   }
 
   private void IngredientRemoved(SelectExitEventArgs args)
   {
-    GameObject obj = args.interactableObject.transform.gameObject;
 
+    GameObject ingredient = args.interactableObject.transform.gameObject;
+    print(string.Format("Removing ingredient {0}", ingredient.name));
+
+    if (ingredient != contents.Peek()) { return; }
+
+    ingredient.transform.SetParent(null);
+
+    GameObject socket = ingredient.transform.Find("StackSocket").gameObject;
+    Destroy(socket);
+
+    contents.Pop();
+
+    if (contents.Count > 0)
+    {
+      GameObject topIngredient = contents.Peek();
+      GameObject topSocket = topIngredient.transform.Find("StackSocket").gameObject;
+
+      EnableSocket(topSocket);
+    }
+    else
+    {
+      EnableSocket(bottomSocket);
+    }
+  }
+
+  private void EnableSocket(GameObject stackSocket)
+  {
+    XRSocketInteractor socket = stackSocket.GetComponent<XRSocketInteractor>();
+    if (socket == null) { return; }
+
+    socket.selectEntered.AddListener(IngredientAdded);
+    socket.selectExited.AddListener(IngredientRemoved);
+    socket.allowHover = true;
+    socket.allowSelect = true;
+  }
+
+  private void DisableSocket(GameObject stackSocket)
+  {
+    XRSocketInteractor socket = stackSocket.GetComponent<XRSocketInteractor>();
+    if (socket == null) { return; }
+
+    socket.selectEntered.RemoveListener(IngredientAdded);
+    socket.selectExited.RemoveListener(IngredientRemoved);
+    socket.allowHover = false;
+    socket.allowSelect = false;
   }
 }
