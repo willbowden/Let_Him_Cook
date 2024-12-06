201c12224ae50991b00d70105b03de582cd5460d
willbowden <willbowden03@yahoo.co.uk>
2024-12-06 11:53:29 +0800

    Add ambient sound and cutting sounds

--- Assets/Scripts/CuttingBoard.cs
+++ Assets/Scripts/CuttingBoard.cs
@@ -13,10 +13,11 @@ public class CuttingBoard : MonoBehaviour
     private ProgressBar progressBar;
     [SerializeField]
     private List<AudioClip> audioClips;
-    
+
     private AudioSource audioSource;
     private GameObject content;
     private int chops = 0;
+    private bool canChop = true;
 
     void Awake()
     {
@@ -54,19 +55,23 @@ public class CuttingBoard : MonoBehaviour
 
     private void Chop()
     {
+        if (audioClips.Count > 0 && audioSource != null)
+        {
+            int index = (int)UnityEngine.Random.Range(0, audioClips.Count);
+            audioSource.PlayOneShot(audioClips[index]);
+        }
 
         int chopsRequired = content.GetComponent<CuttableIngredient>().chopsToCut;
         chops += 1;
         float newSize = (float)chops / chopsRequired;
         progressBar.SetProgress(newSize);
+
         if (chops >= chopsRequired)
         {
             content.transform.GetPositionAndRotation(out var instancePosition, out var instanceRotation);
             GameObject cutPrefab = content.GetComponent<CuttableIngredient>().cutVersionPrefab;
             Destroy(content);
             Instantiate(cutPrefab, new Vector3(0, 0.02f, 0) + instancePosition, transform.rotation * Quaternion.Euler(new Vector3(0, 90, 0)));
-
-            // Don't need to call ResetProgressAndContent() here as it is triggered by IngredientRemoved when we Destroy(content).
         }
     }
 
@@ -74,12 +79,18 @@ public class CuttingBoard : MonoBehaviour
     {
         if (col.gameObject.name.Contains("Knife") && content != null)
         {
-            if (audioClips.Count > 0 && audioSource != null)
+            if (canChop)
             {
-                int index = (int)UnityEngine.Random.Range(0, audioClips.Count);
-                audioSource.PlayOneShot(audioClips[index]);
+                Chop();
+                canChop = false;
+                StartCoroutine(ChopCooldown());
             }
-            Chop();
         }
+    }
+
+    private IEnumerator ChopCooldown()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canChop = true;
     }
 }
